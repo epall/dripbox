5480042ded9cafb8101656fa5bdf2617659f0b82
Eric Allen <ericpallen@gmail.com>
2010-10-17 00:35:01 -0400

    Actually releasing my first Python project

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Dripbox',
+      version='0.1',
+      description='Smart one-way SFTP file synchronization',
+      author='Eric Allen',
+      author_email='eric@hackerengineer.net',
+      maintainer='Eric Allen',
+      maintainer_email='eric@hackerengineer.net',
+      url='http://github.com/epall/dripbox',
+      py_modules=['dripbox'],
+      scripts=['scripts/drip'],
+      requires=['fsevents', 'paramiko'],
+      license='GPL v2.0'
+     )
+
