e48a368bc96a44ee5979471cbc7a35ae2e54f320
Eric Allen <ericpallen@gmail.com>
2010-10-17 01:09:03 -0400

    Figured out how to get dependencies to work

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Dripbox',
-      version='0.1',
+      version='0.1.5',
       description='Smart one-way SFTP file synchronization',
       author='Eric Allen',
       author_email='eric@hackerengineer.net',
@@ -13,6 +13,7 @@ setup(name='Dripbox',
       py_modules=['dripbox'],
       scripts=['scripts/drip'],
       requires=['fsevents', 'paramiko'],
+      install_requires=['MacFSEvents', 'paramiko'],
       license='GPL v2.0'
      )
 
